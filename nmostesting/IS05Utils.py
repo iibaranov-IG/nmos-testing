a8542048995b4d6457e53a773411b8fec84910d5
Igor Baranov <287694593+iibaranov-IG@users.noreply.github.com>
2026-09-02 05:19:07 -0700

    Fix IS-05 SDP session-level connection handling
    Signed-off-by: Igor Baranov <287694593+iibaranov-IG@users.noreply.github.com>

--- nmostesting/IS05Utils.py
+++ nmostesting/IS05Utils.py
@@ -581,6 +581,10 @@ class IS05Utils(NMOSUtils):
                         return False, "SDP destination port {} does not match transport_params: {}" \
                                       .format(media_line.group(2), transport_params["destination_port"])
                     connection_line = re.search(r"c=IN IP[4,6] ([^/\r\n]*)(?:/[0-9]+){0,2}", sdp_data)
+                    if connection_line is None:
+                        connection_line = re.search(r"c=IN IP[4,6] ([^/\r\n]*)(?:/[0-9]+){0,2}", sdp_global)
+                    if connection_line is None:
+                        return False, "SDP does not contain a connection line"
                     if connection_line.group(1) != transport_params["destination_ip"]:
                         return False, "SDP destination IP {} does not match transport_params: {}" \
                                       .format(connection_line.group(1), transport_params["destination_ip"])
